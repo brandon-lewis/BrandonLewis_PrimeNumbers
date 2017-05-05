de05e53a0bee65f0c2a084fcb003ee46b9d0dd70
brandon-lewis <bmlewis357@yahoo.com>
2017-05-04 21:17:06 -0400

    Implemented first generator (pre-abstraction), modified unit tests

--- src/com/lewis/brandon/primes/PrimesGenerator.java
+++ src/com/lewis/brandon/primes/PrimesGenerator.java
@@ -1,21 +1,64 @@
 package com.lewis.brandon.primes;
 
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class PrimesGenerator implements PrimeNumberGenerator {
 	
-	public static void main(String[] args) {
+	private ConcurrentSkipListSet<Integer> primes;
+	private int calculatedUpTo;
+	
+	public static void main(String[] args) throws IOException {
 		PrimesGenerator pg = new PrimesGenerator();
-		pg.generate(20, 1).forEach(System.out::println);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("==========================================");
+		System.out.println("==   Brandon's Prime Number Generator   ==");
+		System.out.println("==========================================");
+		System.out.println("Directions:\n  - Enter a number range (e.g. 7900-7920) to see the prime numbers within that range (inclusive).\n");
+		System.out.print(">> ");
+		String input = br.readLine();
+		while(!input.equals("exit")) {
+			String regexPattern = "^(\\d+)-(\\d+)$";
+			Pattern pattern = Pattern.compile(regexPattern);
+			Matcher matcher = pattern.matcher(input);
+			if(!matcher.find()) {
+				System.out.println("Invalid command.  Enter a number range (e.g. 7900-7920) to see the prime numbers within that range (inclusive).");
+			}
+			int num1 = Integer.valueOf(matcher.group(1));
+			int num2 = Integer.valueOf(matcher.group(2));
+			List<Integer> primes = pg.generate(num1, num2);
+			System.out.println("The Prime Numbers Between " + num1 + " and " + num2 + " are:");
+			System.out.println("\t" + primes.toString().replaceAll("\\[|\\]", "") + "\n");
+			System.out.print(">> ");
+			input = br.readLine();
+		}
+	}
+	
+	private void generatePrimesBySieveOfEratosthenes(int endingValue) {
+		if(endingValue > calculatedUpTo) {
+			// Set up initial set, skipping all even numbers, starting from 3 because 1 is not prime
+			primes = IntStream.iterate(3, (value) -> value + 2)
+					.limit(endingValue/2).boxed().collect(Collectors.toCollection(ConcurrentSkipListSet::new));
+			primes.add(2);	// Add 2 explicitly, as it's the only even prime
+			
+			primes.forEach(num -> {
+				primes.removeIf((targetNum) -> targetNum % num == 0 && targetNum != num);
+			});
+			calculatedUpTo = endingValue;
+		}
 	}
 
 	/**
-	 * Calculates the prime numbers within the given range.  The <i>startingValue</i>
-	 * and <i>endingValue</i> values will be switched if the <i>startingValue</i>
-	 * is larger than the <i>endingValue</i>.
+	 * Calculates the prime numbers within the given range.  The <code>startingValue</code>
+	 * and <code>endingValue</code> values will be switched if the <code>startingValue</code>
+	 * is larger than the <code>endingValue</code>.
 	 * 
 	 * @param startingValue		the number to start generating at (inclusive)
 	 * @param endingValue		the number to stop generating at (inclusive)
@@ -26,23 +69,43 @@ public class PrimesGenerator implements PrimeNumberGenerator {
 		if(startingValue < 1 || endingValue < 1) {
 			throw new IllegalArgumentException("Neither startingValue nor endingValue can be less than 1.");
 		}
+		// Swap startingValue and endingValue if endingValue is greater than startingValue
 		if(startingValue > endingValue) {
 			int tempValue = startingValue;
 			startingValue = endingValue;
 			endingValue = tempValue;
 		}
-		return IntStream.rangeClosed(endingValue, startingValue).filter(this::isPrime)
-				.boxed().collect(Collectors.toCollection(ArrayList::new));
+
+		generatePrimesBySieveOfEratosthenes(endingValue);
+		
+		List<Integer> nums = IntStream.rangeClosed(startingValue, endingValue).boxed().collect(Collectors.toList());
+		nums.removeIf(num -> !isPrime(num));
+		return nums;
 	}
 
+	/**
+	 * Returns a boolean value indicating whether the provided number is prime.
+	 * 
+	 * @param value		the number to be check for primality
+	 * @return			<code>true</code> if the number was determined to be prime; false if not prime
+	 */
 	@Override
 	public boolean isPrime(int value) {
-		if(value <= 1) {
+		if(value < 1) {
+			throw new IllegalArgumentException("Negative numbers not supported, as Prime numbers are non-negative integers greater than 0");
+		} else if(value == 1) {
 			return false;
-		} else if(value == 2) {
+		} else if(value <= 3) {
 			return true;
 		} else {
-			return true;
+			// Run generation method if isPrime() is invoked before 
+			// generate() or if value is outside of previous calculations
+			if(value > calculatedUpTo) {
+				generatePrimesBySieveOfEratosthenes(value);
+				return isPrime(value);
+			} else {
+				return primes.contains(value);
+			}
 		}
 	}
 }
--- src/com/lewis/brandon/primes/PrimesGeneratorTest.java
+++ src/com/lewis/brandon/primes/PrimesGeneratorTest.java
@@ -16,17 +16,8 @@ public class PrimesGeneratorTest {
 	PrimesGenerator pg = new PrimesGenerator();
 
 	// ==================================================
-	// 					isPrime
+	// 					Accuracy Tests
 	// ==================================================
-	@Test
-	public void testRejectsNegativeNumbers() {
-		assertFalse(pg.isPrime(-1));
-	}
-
-	@Test
-	public void testZeroIsNotPrime() {
-		assertFalse(pg.isPrime(0));
-	}
 
 	@Test
 	public void testOneIsNotPrime() {
@@ -35,39 +26,21 @@ public class PrimesGeneratorTest {
 
 	@Test
 	public void test7900IsNotPrime() {
+		pg.generate(1, 7900);
 		assertFalse(pg.isPrime(7900));
 	}
 
 	@Test
 	public void test7919IsPrime() {
+		pg.generate(1, 7920);
 		assertTrue(pg.isPrime(7919));
 	}
 
-	
-	// ==================================================
-	// 					generate
-	// ==================================================
 	@Test
-	public void testGeneratorHandlesInverseRanges() {
-		List<Integer> expected = Arrays.asList(2,3,4,7);
-		List<Integer> primes = pg.generate(10, 1);
-		assertTrue(primes.equals(expected));
-	}
-	@Test
-	public void testGeneratorInverseRangesReturnsSameResultsAsStandardRanges() {
-		List<Integer> standard = pg.generate(15, 30);
-		List<Integer> inverse = pg.generate(30, 15);
-		assertTrue(standard.equals(inverse));
-	}
-
-	@Test(expected=IllegalArgumentException.class)
-	public void testGeneratorRejectsANegativeStartingValue() {
-		pg.generate(-10, 1000);
-	}
-
-	@Test(expected=IllegalArgumentException.class)
-	public void testGeneratorRejectsANegativeEndingValue() {
-		pg.generate(10, -1000);
+	public void testGenerateFindsFirst26Primes() {
+		List<Integer> expected = Arrays.asList(2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101);
+		List<Integer> results = pg.generate(1, 101);
+		assertTrue(results.equals(expected));
 	}
 
 	@Test
@@ -75,5 +48,44 @@ public class PrimesGeneratorTest {
 		List<Integer> expected = Arrays.asList(7901, 7907, 7919);
 		List<Integer> results = pg.generate(7900, 7920);
 		assertTrue(results.equals(expected));
+	}
+
+	// ==================================================
+	// 					isPrime
+	// ==================================================
+	@Test(expected=IllegalArgumentException.class)
+	public void testIsPrimeRejectsNegativeNumbers() {
+		assertFalse(pg.isPrime(-84));
+	}
+
+	@Test
+	public void testIsPrimeInvokedBeforeGenerateCalculatesProperly() {
+		assertTrue(pg.isPrime(7919));
+	}
+	
+	// ==================================================
+	// 					generate
+	// ==================================================
+	@Test
+	public void testGeneratorHandlesInverseRanges() {
+		List<Integer> expected = Arrays.asList(2,3,5,7);
+		List<Integer> primes = pg.generate(10, 1);
+		assertTrue(primes.equals(expected));
+	}
+	@Test
+	public void testGeneratorInverseRangesReturnsSameResultsAsStandardRanges() {
+		List<Integer> standard = pg.generate(1, 200);
+		List<Integer> inverse = pg.generate(200, 1);
+		assertTrue(standard.equals(inverse));
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void testGeneratorRejectsNegativeStartingValue() {
+		pg.generate(-10, 1000);
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void testGeneratorRejectsNegativeEndingValue() {
+		pg.generate(10, -1000);
 	}
 }
